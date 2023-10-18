07bf858458ee2f3a614ba88393265e811d3f437b
Johnwchampion <146457237+Johnwchampion@users.noreply.github.com>
2023-10-18 23:58:15 +0000

    hello please worksheet1

--- week3_c/worksheet1.c
+++ week3_c/worksheet1.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 
 int main(){
-    /*
-    int number;
+    /*BRONZE1*/
+    /*int number;
     scanf("%d", &number);
     if(number<0)
         printf("negative number");
     else if(number == 0)
         printf("your number is zero");
     else    
-        printf("positive number");
-
-
+        printf("positive number"); */ 
     
-    int number;
+    
+    /*BRONZE2*/
+    /*int number;
     scanf("%d", &number);
     if(number%4 == 0)
         printf("your number is divisble by 4 \n");
@@ -22,8 +22,11 @@ int main(){
     else
         printf("your number is divisble by neither 4 nor 5 \n");
 
-    return 0;
+    return 0;*/ 
     
+    
+    /*Silver1*/
+    /*
     float mark;
     printf("what grade did you get bro? \n");
     scanf("%f", &mark);
@@ -37,11 +40,15 @@ int main(){
     else    
         printf("that mark ain't valid");
 
-    return 0;
+    return 0;*/
+     
 
-    */
+    
+ 
 
-float temperature;
+/*Silver2*/ 
+
+/*float temperature;
  printf("what's the temperature ? \n");
  scanf("%f", &temperature);
 
@@ -49,5 +56,26 @@ if(temperature >= -10 && temperature <= 40)
     printf("you are experiencing a temperature which lies somewhewre between -10 and 40 degrees celcius \n");
 else
     printf("it is either colder than -10 degrees or hotter than 40 degrees \n");
-return 0;
+return 0;*/
+
+/*Gold1*/
+
+int choices;
+choices = {0,1,2,3,4};
+switch(choices){
+scanf('%d');
+    case 0:
+        printf("you obtained 0 \n");
+    case 1:
+        printf("you obtained 1 \n");
+    case 2:
+        printf("you obtained 2 \n");
+    case 3:
+        printf("you obtained 3 \n");
+    case 4:
+        printf("you obtained 4 \n");
+    default:
+        printf("out of range \n");
 }
+
+    return 0; }
